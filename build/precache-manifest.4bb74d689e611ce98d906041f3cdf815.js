03f87d7d84ada26e478bba90e83cfe2ba23306d3
mildsummer <mild.summer.y@gmail.com>
2020-06-02 18:34:45 +0900

    buildディレクトリをコミット

---
+++ build/precache-manifest.4bb74d689e611ce98d906041f3cdf815.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd8238b010e66f94443e51c05547811",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ab20c804c68d7f61619",
+    "url": "/static/css/main.22766995.chunk.css"
+  },
+  {
+    "revision": "698a5952a46452276f48",
+    "url": "/static/js/2.e785a15f.chunk.js"
+  },
+  {
+    "revision": "0fb0e672f1308cb5b7677cdb93fc42ab",
+    "url": "/static/js/2.e785a15f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ab20c804c68d7f61619",
+    "url": "/static/js/main.1c60346f.chunk.js"
+  },
+  {
+    "revision": "aed94f6106ee79fd88bc",
+    "url": "/static/js/runtime-main.0f2f2e4a.js"
+  }
+]);
